ef70053ea2bed974afb8e7a433cdb3bd6872a5d9
Ian Vincent Slesser <ivslesser@Belindas-iMac.lan>
2019-05-28 12:12:26 +0100

    ImGUI

--- premake5.lua
+++ premake5.lua
@@ -28,7 +28,6 @@ project "Particle"
         "vendor/glad/include",
         "vendor/glm",
         "vendor/spdlog/include",
-        "vendor/imgui"
     }
 
     libdirs {
@@ -42,8 +41,8 @@ project "Particle"
         links {
             "glfw3",
             "opengl32",
-            "ImGui"
         }
+        
         defines
         {
             "PT_WIN"
@@ -130,29 +129,4 @@ project "ParticleTest"
     links { 
         "GoogleTest",
         "Particle"
-    }
-
-project "ImGui"
-    kind "StaticLib"
-    language "C++"
-    systemversion "latest"
-    cppdialect "C++17"
-    staticruntime "On"
-    location "ImGui"
-
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-    files
-    {
-        "vendor/imgui/imconfig.h",
-        "vendor/imgui/imgui.h",
-        "vendor/imgui/imgui.cpp",
-        "vendor/imgui/imgui_draw.cpp",
-        "vendor/imgui/imgui_internal.h",
-        "vendor/imgui/imgui_widgets.cpp",
-        "vendor/imgui/imstb_rectpack.h",
-        "vendor/imgui/imstb_textedit.h",
-        "vendor/imgui/imstb_truetype.h",
-        "vendor/imgui/imgui_demo.cpp"
     }
